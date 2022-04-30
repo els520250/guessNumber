521f330c5759820e5cdd62259d6f226f4687ae53
Els <els52025099@gmail.com>
2022-04-30 19:14:25 +0900

    final verson

--- guessNumber.py
+++ guessNumber.py
@@ -1,14 +1,22 @@
 import random
 
-number = random.randint(1,100)
+
+start = input('請輸入範圍起始值: ')
+end = input('請輸入範圍終止值: ')
+start = int(start)
+end = int(end)
+number = random.randint(start,end)
+
+count = 0
 #print(number)
 while True:
-	guess = input('輸入數字1~100: ')
+	guess = input('輸入數字: ')
 	guess = int(guess)
+	count += 1
 	if guess == number:
-		print('猜中數字')
+		print('猜中數字! 你猜了', count, '次')
 		break
 	elif guess > number:
-		print('比', guess, '還要小')
+		print('比', guess, '還要小,你猜了', count, '次')
 	elif guess < number:
-		print('比', guess, '還要大')
+		print('比', guess, '還要大,你猜了', count, '次')
