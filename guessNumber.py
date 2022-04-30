5efbaeaff1cb049a9a17e292140b174cbc784169
Els <els52025099@gmail.com>
2022-04-30 18:51:48 +0900

    upload guessNumber.py

---
+++ guessNumber.py
@@ -0,0 +1,16 @@
+import random
+
+number = random.randint(1,100)
+#print(number)
+while True:
+	guess = input('輸入數字1~100: ')
+	guess = int(guess)
+	if guess == number:
+		print('猜中數字')
+		break
+	elif guess > number:
+		print('比', guess, '還要小')
+	elif guess < number:
+		print('比', guess, '還要大')
+	else:
+		print('請輸入數字')
